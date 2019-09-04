7f891ea5dc2639d7eeaf5286fb87ae511cff6e04
Jmt1995 <841540470@qq.com>
2019-09-04 22:05:28 +0800

    second

--- jmt/settings.py
+++ jmt/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'jmt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # LOGIN_URL = '/index'
+
+# python3 manage.py runserver 0.0.0.0:8080
